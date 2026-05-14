2bb15c5ab8c4c14044be7123e0ca4c81b8348759
Justin Ray Angus <angus1@llnl.gov>
2026-05-14 12:39:35 -0700

    bug fix in ApplyInverseVolumeScaling (#6856)

--- Examples/Tests/ohm_solver_em_modes/analysis_rz.py
+++ Examples/Tests/ohm_solver_em_modes/analysis_rz.py
@@ -179,5 +179,5 @@ else:
     amps = np.abs(F_kw[2, 1, len(kz) // 2 - 2 : len(kz) // 2 + 2])
     print("Amplitude sample: ", repr(amps))
     assert np.allclose(
-        amps, np.array([55.65891974, 31.29213566, 70.13683876, 15.395433])
+        amps, np.array([55.6719968, 31.26152007, 70.14344043, 15.4179245])
     )
