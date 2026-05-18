Helion-Energy/WarpX

commit aa7b1330e99a69785faa6057bd846c946c124d34

use time-centered E from previous step as initial guess for implicit solvers. (#6784)
This PR updates the initial guess for the electric field in the
nonlinear solve of the implicit evolve schemes. Instead of using the
electric field at the end of the previous time step, it now uses the
time-centered electric field from the previous step. This is consistent
with how particles are advanced, which already uses the time-centered
field.

In regimes with high plasma pressure and low particles per cell (e.g.,
near the axis of a Z-pinch at stagnation in cylindrical geometry),
electric field fluctuations can be large. Using the end-of-step field as
an initial guess can amplify these fluctuations, leading to unphysically
large particle orbits and eventual solver divergence.

The time-centered electric field exhibits significantly smaller
fluctuations, providing a more stable and physically consistent initial
guess.

This change resolves the issue reported in PR #6738, where the 1D
RCYLINDER Z-pinch benchmark fails when particle suborbits are enabled.

Source/Diagnostics/WarpXIO.cpp
@@ -304,6 +304,15 @@ WarpX::InitFromCheckpoint ()
             }
         }
 
+        if (m_fields.has_vector(FieldType::E_old, lev)) {
+            VisMF::Read(*m_fields.get(FieldType::E_old, Direction{0}, lev),
+                        amrex::MultiFabFileFullPrefix(lev, restart_chkfile, level_prefix, "Ex_old"));
+            VisMF::Read(*m_fields.get(FieldType::E_old, Direction{1}, lev),
+                        amrex::MultiFabFileFullPrefix(lev, restart_chkfile, level_prefix, "Ey_old"));
+            VisMF::Read(*m_fields.get(FieldType::E_old, Direction{2}, lev),
+                        amrex::MultiFabFileFullPrefix(lev, restart_chkfile, level_prefix, "Ez_old"));
+        }
+
         VisMF::Read(*m_fields.get(FieldType::Efield_fp, Direction{0}, lev),
                     amrex::MultiFabFileFullPrefix(lev, restart_chkfile, level_prefix, "Ex_fp"));
         VisMF::Read(*m_fields.get(FieldType::Efield_fp, Direction{1}, lev),
@@ -412,8 +421,7 @@ WarpX::InitFromCheckpoint ()
     mypc->Restart(restart_chkfile);
 
     if (m_implicit_solver) {
-
-        m_implicit_solver->Define(this);
+        m_implicit_solver->Define(this, restart_chkfile.empty());
         m_implicit_solver->CreateParticleAttributes();
     }
 

Source/Diagnostics/FlushFormats/FlushFormatCheckpoint.cpp
@@ -86,6 +86,14 @@ FlushFormatCheckpoint::WriteToFile (
                      amrex::MultiFabFileFullPrefix(lev, checkpointname, default_level_prefix, "Ey_fp"));
         VisMF::Write(*warpx.m_fields.get(FieldType::Efield_fp, Direction{2}, lev),
                      amrex::MultiFabFileFullPrefix(lev, checkpointname, default_level_prefix, "Ez_fp"));
+        if (warpx.m_fields.has_vector(FieldType::E_old, lev)) {
+            VisMF::Write(*warpx.m_fields.get(FieldType::E_old, Direction{0}, lev),
+                         amrex::MultiFabFileFullPrefix(lev, checkpointname, default_level_prefix, "Ex_old"));
+            VisMF::Write(*warpx.m_fields.get(FieldType::E_old, Direction{1}, lev),
+                         amrex::MultiFabFileFullPrefix(lev, checkpointname, default_level_prefix, "Ey_old"));
+            VisMF::Write(*warpx.m_fields.get(FieldType::E_old, Direction{2}, lev),
+                         amrex::MultiFabFileFullPrefix(lev, checkpointname, default_level_prefix, "Ez_old"));
+        }
         VisMF::Write(*warpx.m_fields.get(FieldType::Bfield_fp, Direction{0}, lev),
                      amrex::MultiFabFileFullPrefix(lev, checkpointname, default_level_prefix, "Bx_fp"));
         VisMF::Write(*warpx.m_fields.get(FieldType::Bfield_fp, Direction{1}, lev),

Source/Diagnostics/FlushFormats/FlushFormatPlotfile.cpp
@@ -636,6 +636,15 @@ FlushFormatPlotfile::WriteAllRawFields(
                     default_level_prefix, "By_fp", lev, plot_raw_fields_guards );
         WriteRawMF( *warpx.m_fields.get(FieldType::Bfield_fp, Direction{2}, lev), dm, raw_pltname,
                     default_level_prefix, "Bz_fp", lev, plot_raw_fields_guards );
+        if (warpx.m_fields.has_vector(FieldType::E_old, lev))
+        {
+            WriteRawMF( *warpx.m_fields.get(FieldType::E_old, Direction{0}, lev), dm, raw_pltname,
+                        default_level_prefix, "Ex_old", lev, plot_raw_fields_guards );
+            WriteRawMF( *warpx.m_fields.get(FieldType::E_old, Direction{1}, lev), dm, raw_pltname,
+                        default_level_prefix, "Ey_old", lev, plot_raw_fields_guards );
+            WriteRawMF( *warpx.m_fields.get(FieldType::E_old, Direction{2}, lev), dm, raw_pltname,
+                        default_level_prefix, "Ez_old", lev, plot_raw_fields_guards );
+        }
         if (warpx.m_fields.has(FieldType::F_fp, lev))
         {
             WriteRawMF( *warpx.m_fields.get(FieldType::F_fp, lev), dm, raw_pltname,

Source/FieldSolver/ImplicitSolvers/ImplicitSolver.H
@@ -48,7 +48,7 @@ public:
      * \brief Read user-provided parameters that control the implicit solver.
      * Allocate internal arrays for intermediate field values needed by the solver.
      */
-    virtual void Define ( WarpX*  a_WarpX ) = 0;
+    virtual void Define (WarpX* a_WarpX, bool from_restart) = 0;
 
     [[nodiscard]] bool IsDefined () const { return m_is_defined; }
 
@@ -250,6 +250,8 @@ protected:
      * \brief parse nonlinear solver parameters (if one is used)
      */
     void parseNonlinearSolverParams ( const amrex::ParmParse&  pp );
+
+    void SaveEoldMultifab ();
 
     /**
      * \brief Initialize the Mass Matrices used for plasma response in nonlinear Newton solver

Source/FieldSolver/ImplicitSolvers/ImplicitSolver.cpp
@@ -529,6 +529,18 @@ void ImplicitSolver::parseNonlinearSolverParams ( const amrex::ParmParse&  pp )
 
 }
 
+void ImplicitSolver::SaveEoldMultifab ()
+{
+    // E_old multifab is needed for diagnostics and saving at checkpoints
+    for (int lev = 0; lev < m_num_amr_levels; ++lev) {
+        const ablastr::fields::VectorField Efp = m_WarpX->m_fields.get_alldirs(FieldType::Efield_fp, lev);
+        ablastr::fields::VectorField E_old = m_WarpX->m_fields.get_alldirs(FieldType::E_old, lev);
+        for (int n = 0; n < 3; ++n) {
+            amrex::MultiFab::Copy(*E_old[n], *Efp[n], 0, 0, E_old[n]->nComp(), E_old[n]->nGrowVect());
+        }
+    }
+}
+
 void ImplicitSolver::InitializeMassMatrices ()
 {
 

Source/FieldSolver/ImplicitSolvers/SemiImplicitEM.H
@@ -53,7 +53,7 @@ public:
     SemiImplicitEM(SemiImplicitEM&&) = delete;
     SemiImplicitEM& operator=(SemiImplicitEM&&) = delete;
 
-    void Define ( WarpX*  a_WarpX ) override;
+    void Define (WarpX* a_WarpX, bool a_from_restart) override;
 
     void PrintParameters () const override;
 

Source/FieldSolver/ImplicitSolvers/SemiImplicitEM.cpp
@@ -12,7 +12,7 @@
 using warpx::fields::FieldType;
 using namespace amrex::literals;
 
-void SemiImplicitEM::Define ( WarpX*  a_WarpX )
+void SemiImplicitEM::Define (WarpX*  a_WarpX, bool  a_from_restart)
 {
     BL_PROFILE("SemiImplicitEM::Define()");
 
@@ -24,12 +24,16 @@ void SemiImplicitEM::Define ( WarpX*  a_WarpX )
     m_WarpX = a_WarpX;
 
     // Define E and Eold vectors
-    m_E.Define( m_WarpX, "Efield_fp" );
-    m_Eold.Define( m_E );
+    m_E.Define(m_WarpX, "Efield_fp");
+    m_Eold.Define(m_E);
+
+    // Set initial values for E and Eold vectors
+    m_E.Copy(FieldType::Efield_fp);
+    m_Eold.Copy(a_from_restart ? FieldType::E_old : FieldType::Efield_fp, FieldType::None, true);
 
     // Parse implicit solver parameters
     const amrex::ParmParse pp("implicit_evolve");
-    parseNonlinearSolverParams( pp );
+    parseNonlinearSolverParams(pp);
 
     // Define the nonlinear solver
     m_nlsolver->Define(m_E, this);
@@ -68,10 +72,15 @@ void SemiImplicitEM::OneStep ( amrex::Real  start_time,
     // Particles have up^{n} and xp^{n}.
 
     // Save up and xp at the start of the time step
-    m_WarpX->SaveParticlesAtImplicitStepStart ( );
+    m_WarpX->SaveParticlesAtImplicitStepStart();
 
-    // Save Eg at the start of the time step
-    m_Eold.Copy( FieldType::Efield_fp );
+    // Initial guess for Eg^{n+theta} is Eg^{n-1+theta}
+    // (i.e., Eg used to advance the system from step n-1 to step n)
+    m_E.linComb(1.0_rt - m_theta, m_Eold, m_theta, m_E);
+
+    // Save Eg at start of time step
+    SaveEoldMultifab();
+    m_Eold.Copy(FieldType::E_old, FieldType::None, true);
 
     // Advance WarpX owned Bfield_fp from t_{n} to t_{n+1/2}
     m_WarpX->EvolveB(0.5_rt*m_dt, SubcyclingHalf::FirstHalf, start_time);
@@ -81,11 +90,10 @@ void SemiImplicitEM::OneStep ( amrex::Real  start_time,
 
     // Solve nonlinear system for Eg at t_{n+1/2}
     // Particles will be advanced to t_{n+1/2}
-    m_E.Copy(m_Eold); // initial guess for Eg^{n+1/2}
-    m_nlsolver->Solve( m_E, m_Eold, start_time, m_dt, a_step );
+    m_nlsolver->Solve(m_E, m_Eold, start_time, m_dt, a_step);
 
     // Update WarpX owned Efield_fp to t_{n+1/2}
-    m_WarpX->SetElectricFieldAndApplyBCs( m_E, half_time );
+    m_WarpX->SetElectricFieldAndApplyBCs(m_E, half_time);
     m_WarpX->reduced_diags->ComputeDiagsMidStep(a_step);
 
     // Advance particles from time n+1/2 to time n+1
@@ -93,7 +101,7 @@ void SemiImplicitEM::OneStep ( amrex::Real  start_time,
 
     // Advance Eg from time n+1/2 to time n+1
     // Eg^{n+1} = 2.0*Eg^{n+1/2} - Eg^n
-    m_E.linComb( 2._rt, m_E, -1._rt, m_Eold );
+    m_E.linComb(2._rt, m_E, -1._rt, m_Eold);
     const amrex::Real new_time = start_time + m_dt;
     m_WarpX->SetElectricFieldAndApplyBCs( m_E, new_time );
 

Source/FieldSolver/ImplicitSolvers/StrangImplicitSpectralEM.H
@@ -54,7 +54,7 @@ public:
     StrangImplicitSpectralEM(StrangImplicitSpectralEM&&) = delete;
     StrangImplicitSpectralEM& operator=(StrangImplicitSpectralEM&&) = delete;
 
-    void Define ( WarpX*  a_WarpX ) override;
+    void Define (WarpX* a_WarpX, bool a_from_restart) override;
 
     void PrintParameters () const override;
 

commit 4b6554fee54032d85e3b499325f8a31ce4197463

implicit restart bug fix (#6864)
This PR fixes a bug introduced in PR #6784. The `from_restart` flag
passed to `m_implicit->Define()` should be `true` when called from
`WarpX::InitFromCheckpoint()`.

Source/Diagnostics/WarpXIO.cpp
@@ -421,7 +421,7 @@ WarpX::InitFromCheckpoint ()
     mypc->Restart(restart_chkfile);
 
     if (m_implicit_solver) {
-        m_implicit_solver->Define(this, restart_chkfile.empty());
+        m_implicit_solver->Define(this, /*from_restart=*/true);
         m_implicit_solver->CreateParticleAttributes();
     }
 