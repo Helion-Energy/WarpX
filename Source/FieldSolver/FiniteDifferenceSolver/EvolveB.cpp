83d96ab2d3225e3cc4467fdaeebf2922859b7cd5
S. Eric Clark <245461744+clarkse-he@users.noreply.github.com>
2026-07-06 18:05:26 -0700

    Revert "Hybrid-PIC EB: strip the EM-ECT machinery to the eb_ect_hardening branch"
    This reverts commit 58c21334bcb4dac362ae2f2fde15e311d88cf92d.

--- Source/FieldSolver/FiniteDifferenceSolver/EvolveB.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/EvolveB.cpp
@@ -107,6 +107,13 @@ void FiniteDifferenceSolver::EvolveB (
 
         EvolveBCartesian <CartesianNodalAlgorithm> ( Bfield, Efield, Gfield, lev, dt );
 
+    } else if (m_fdtd_algo == ElectromagneticSolverAlgo::ECT ||
+               (m_fdtd_algo == ElectromagneticSolverAlgo::HybridPIC &&
+                WarpX::UseConformalEBSolve())) {
+
+        EvolveBCartesianECT(Bfield, face_areas, area_mod, ECTRhofield, Venl, flag_info_cell,
+                            borrowing, lev, dt);
+
     } else if ((m_fdtd_algo == ElectromagneticSolverAlgo::Yee) ||
                (m_fdtd_algo == ElectromagneticSolverAlgo::HybridPIC)) {
 
@@ -115,9 +122,6 @@ void FiniteDifferenceSolver::EvolveB (
     } else if (m_fdtd_algo == ElectromagneticSolverAlgo::CKC) {
 
         EvolveBCartesian <CartesianCKCAlgorithm> ( Bfield, Efield, Gfield, lev, dt );
-    } else if (m_fdtd_algo == ElectromagneticSolverAlgo::ECT) {
-        EvolveBCartesianECT(Bfield, face_areas, area_mod, ECTRhofield, Venl, flag_info_cell,
-                            borrowing, lev, dt);
 #endif
     } else {
         WARPX_ABORT_WITH_MESSAGE("EvolveB: Unknown algorithm");
